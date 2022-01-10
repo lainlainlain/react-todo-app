0ebbef18b248d3f1403b9e8f10ff18e101e9d213
asdqwee <layerlain@yandex.ru>
2022-01-11 07:57:03 +0900

    start

---
+++ src/components/List/List.js
@@ -0,0 +1,24 @@
+import "./List.scss";
+
+const List = ({ props }) => {
+  return (
+    <ul className="list">
+      {props.map((item) => {
+        return (
+          <li className={item.active ? 'active' : ''}>
+            <i>
+              {item.icon ? (
+                item.icon
+              ) : (
+                <i className={`badge badge--${item.color}`}></i>
+              )}
+            </i>
+            <span>{item.name}</span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default List;
